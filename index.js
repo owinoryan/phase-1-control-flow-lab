b698fceb02f33a5e6c0eb3004350ae8acaee284a
ryanowino <owinoryan@gmail.com>
2024-07-01 11:31:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(distance) {
+  let result;
+
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance > 400 && distance < 2000) {
+    result = "That will be twenty bucks.";
+  } else if (distance>2000 && distance<=2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city) {
+  const result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "thanks for everything":
+      return "Bye.";
+  }
 }
